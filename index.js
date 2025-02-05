547571a69f57bb5b1a6d8abdfb4958cb88ce515f
Amit <amitkumarmandal2111@gmail.com>
2025-02-05 16:57:22 +0530

    (feat): re-triggering the deployment

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.get("/" , async ( req , res) => {
 app.get("/user/all" ,async(req , res) => {
     return res.json([{name :"Amit"},{ name : "Akash"} ]);
 })
+app.get("/health" , async(req , res) => {
+    res.json({ok : 200});
+})
 app.listen(PORT , () => console.log(`Running on port ${PORT}`))
