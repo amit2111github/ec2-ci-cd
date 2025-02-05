7afa9e25d05931d471d31c06bda61cd579db4e09
Amit <amitkumarmandal2111@gmail.com>
2025-02-05 19:01:39 +0530

    (feat): new endpoint for params

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const app = require("express")();
 
 const PORT = 3000;
 
-app.get("/" , async ( req , res) => {
-  return res.json({name : "Amit"})  
-})
-app.get("/user/all" ,async(req , res) => {
-    return res.json([{name :"Amit"},{ name : "Akash"} ]);
-})
-app.get("/health" , async(req , res) => {
-    res.json({ok : 200});
-})
-app.get("/amit" , async (req ,res) => {
-    return res.json({msg : "Response from amit"});
-})
-app.listen(PORT , () => console.log(`Running on port ${PORT}`))
+app.get("/", async (req, res) => {
+  return res.json({ name: "Amit" });
+});
+app.get("/user/all", async (req, res) => {
+  return res.json([{ name: "Amit" }, { name: "Akash" }]);
+});
+app.get("/health", async (req, res) => {
+  res.json({ ok: 200 });
+});
+app.get("/amit", async (req, res) => {
+  return res.json({ msg: "Response from amit" });
+});
+app.get("/params/:name", (req, res) => {
+  const { name } = req.params;
+  return res.json({ name });
+});
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
